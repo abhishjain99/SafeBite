b588d60113f86cc1192235c51a8c3960f6961f20
Vaishnav Nagarajan <vaishnav2397@outlook.com>
2024-03-30 23:24:40 -0400

    ingredients button

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from taipy.gui import Gui
 # cover image
 bg = "/images/photo.jpeg"
 
-# path of image to be uploaded
-path = None
-# image data blob
-data = None
+# path of images to be uploaded (qr + ingredients)
+path1 = None
+path2 = None
+
+# image data blob (qr + ingredients)
+data1 = None
+data2 = None
 
 # page markdown
 md = """
@@ -14,19 +17,28 @@ md = """
 # SafeByte
 <|{bg}|image|>
 #### SafeByte is an innovative application designed to promote healthier eating habits. By simply scanning the ingredients of a food item, SafeByte swiftly analyzes its nutritional content and alerts users to its health status. Through a user-friendly interface, it provides clear indications of whether the food is healthy or not, empowering individuals to make informed dietary choices on the go.
-<|{path}|file_selector|label=Upload QR code|on_action=load_qr|extensions=.jpg,.jpeg,.png|>
+<|{path1}|file_selector|label=Upload QR code|on_action=load_qr|extensions=.jpg,.jpeg,.png|>
+
+<|{path2}|file_selector|label=Upload Ingredients|on_action=load_ingredients|extensions=.jpg,.jpeg,.png|>
 
 <|content|>
 This application was created with [Taipy](https://www.taipy.io/).
 >
 """
 
-# load_qr into memory
+# load qr image into memory
 def load_qr(state):
-    with open(state.path, "rb") as f:
-      state.data = f.read()
+    with open(state.path1, "rb") as f:
+      state.data1 = f.read()
       # do whatever has to be done with the qr image here
-      print(state.data)
+      print(state.data1)
+
+# load ingredients image into memory
+def load_ingredients(state):
+    with open(state.path2, "rb") as f:
+      state.data2 = f.read()
+      # do whatever has to be done with the ingredients image here
+      print(state.data2)
 
 # invokes taipy
 Gui(md).run()
